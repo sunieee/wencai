e6121e084c644958c457d7a8b794171791ca2da5
sunie <1287495769@qq.com>
2021-10-04 14:05:00 +0800

    add OCR

--- wencai/torch/OCR.py
+++ wencai/torch/OCR.py
@@ -1,51 +1,43 @@
+from PIL import Image
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import Dataset, DataLoader
 from torchvision.transforms.functional import to_tensor, to_pil_image
 
-from captcha.image import ImageCaptcha
-from tqdm import tqdm
-import random
-import numpy as np
-from collections import OrderedDict
+from captcha_break.code.utils import Char, CaptchaDataset
+import os
+import sys
 
-import string
-characters = '-' + string.digits + string.ascii_uppercase
-width, height, n_len, n_classes = 192, 64, 4, len(characters)
+lowercase = True
+
+img = Image.open('/tmp/wencai/tmp.png')
+print(img.size)
+
+pil_to_tensor()
+char = Char(lowercase)
+width, height, n_len, n_classes = 192, 64, 4, char.length  # 192 64
 n_input_length = 12
-print(characters, width, height, n_len, n_classes)
+print(char.characters, width, height, n_len, n_classes)
 
 
-class CaptchaDataset(Dataset):
-    def __init__(self, characters, length, width, height, input_length, label_length):
-        super(CaptchaDataset, self).__init__()
-        self.characters = characters
-        self.length = length
-        self.width = width
-        self.height = height
-        self.input_length = input_length
-        self.label_length = label_length
-        self.n_class = len(characters)
-        self.generator = ImageCaptcha(width=width, height=height)
-
-    def __len__(self):
-        return self.length
-
-    def __getitem__(self, index):
-        random_str = ''.join([random.choice(self.characters[1:])
-                             for j in range(self.label_length)])
-        image = to_tensor(self.generator.generate_image(random_str))
-        target = torch.tensor([self.characters.find(x)
-                              for x in random_str], dtype=torch.long)
-        input_length = torch.full(
-            size=(1, ), fill_value=self.input_length, dtype=torch.long)
-        target_length = torch.full(
-            size=(1, ), fill_value=self.label_length, dtype=torch.long)
-        return image, target, input_length, target_length
-
-
-dataset = CaptchaDataset(characters, 1, width, height, n_input_length, n_len)
+dataset = CaptchaDataset(char, 1, width, height, n_input_length, n_len)
 image, target, input_length, label_length = dataset[0]
-print(''.join([characters[x] for x in target]), input_length, label_length)
+print(''.join([char.characters[x]
+               for x in target]), input_length, label_length)
+to_pil_image(image)
+
+filepath = os.path.dirname(os.path.abspath(__file__))  # 1. 源文件夹
+sys.path.append(f'{filepath}/captcha_break/code/')
+model = torch.load(f'{filepath}/captcha_break/model/ctc_lower_2021.pth')
+
+model.eval()
+output = model(image.unsqueeze(0).cuda())
+output_argmax = output.detach().permute(1, 0, 2).argmax(dim=-1)
+do = True
+while do or char.decode_target(target) == char.decode(output_argmax[0]):
+    do = False
+    image, target, input_length, label_length = dataset[0]
+    print('true:', char.decode_target(target))
+
+    output = model(image.unsqueeze(0).cuda())
+    output_argmax = output.detach().permute(1, 0, 2).argmax(dim=-1)
+    print('pred:', char.decode(output_argmax[0]))
 to_pil_image(image)
